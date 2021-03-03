d3a84df538bb1f434602aa0b00e916d693da4279
Rafa <rafagabidulin@gmail.com>
2021-03-03 21:26:12 +0300

    Это на первый коммит

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//  let answer = +prompt("Есть ли вам 18?", "Да");
+
+//  console.log(typeof(answer));
+ 
+ 
+//  console.log("arr" + " - object");
+
+//  console.log(4 + " - object");
+
+// let incr = 10;
+// let decr = 10;
+
+// console.log(incr++);
+// console.log(decr--);
+
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
